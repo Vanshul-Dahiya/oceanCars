c8fb18b3eb61b844d29d016f317fcb2d86accf00
Vanshul Dahiya <vanshuldahiya2002@gmail.com>
2023-12-15 12:47:43 +0530

    data rendered on server side + searchParams

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from 'react'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { SearchManufacturer } from '.'
 import Image from 'next/image'
@@ -14,7 +15,33 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 const SearchBar = () => {
     const [manufacturer, setManufacturer] = useState('')
     const [model, setModel] = useState('')
-    const handleSearch = () => {
+    const router = useRouter()
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (manufacturer === '' && model === '') {
+            return alert("Please fill in the search bar")
+        }
+
+        updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+    }
+    const updateSearchParams = (model: string, manufacturer: string) => {
+        const searchParams = new URLSearchParams(window.location.search);
+
+        if (model) {
+            searchParams.set('model', model);
+        } else {
+            searchParams.delete('model')
+        }
+        if (manufacturer) {
+            searchParams.set('manufacturer', manufacturer);
+        } else {
+            searchParams.delete('manufacturer')
+        }
+
+        const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+        router.push(newPathname)
 
     }
     return (
--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     "X-RapidAPI-Key": "e82dca1c2amshd95ab015a1ec42bp1f1eb2jsn825e7a6b7d54",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&feul_type=${fuel}`,
     { headers: headers }
   );
   const result = await response.json();
